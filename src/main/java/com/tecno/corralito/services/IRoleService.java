1bbf289e0f346880a81f2ce8f2f24c798dc64d6a
Daniel <dcardales260@gmail.com>
2024-10-03 18:27:57 -0500

    register y login de  usuario sin entidades

--- src/main/java/com/tecno/corralito/services/IRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.tecno.corralito.services;
-
-import com.tecno.corralito.models.entities.enums.RoleEnum;
-import com.tecno.corralito.models.entities.usuario.RoleEntity;
-
-
-public interface IRoleService {
-    RoleEntity getRoleByEnum(RoleEnum roleEnum);
-}
